31102814de24de770bd0eb95793f560d855bca7a
owfts <130887660+owfts@users.noreply.github.com>
2023-06-12 00:22:41 +0600

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./FooterStyles.css";
+import {
+	FaFacebook,
+	FaHome,
+	FaLinkedin,
+	FaMailBulk,
+	FaPhone,
+	FaTwitter,
+} from "react-icons/fa";
+
+const Footer = () => {
+	return (
+		<div className='footer'>
+			<div className='footer-container'>
+				<div className='left'>
+					<div className='location'>
+						<FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+						<div>
+							<p>Unlearned from professionals.</p>
+							<p>Okurmen.</p>
+						</div>
+					</div>
+					<div className='phone'>
+						<h4>
+							<FaPhone
+								size={20}
+								style={{ color: "#fff", marginRight: "2rem" }}
+							/>
+							+996 701-556-421
+						</h4>
+					</div>
+
+					<div className='email'>
+						<h4>
+							<FaMailBulk
+								size={20}
+								style={{ color: "#fff", marginRight: "2rem" }}
+							/>
+							info@gmail.com
+						</h4>
+					</div>
+				</div>
+
+				<div className='right'>
+					<h4>About the company</h4>
+					<p>
+						This is me Bakytbekova Dayana. I enjoy discussing new projects and
+						design challenges.
+					</p>
+					<div className='social'>
+						<FaFacebook
+							size={30}
+							style={{ color: "#fff", marginRight: "1rem" }}
+						/>
+						<FaTwitter
+							size={30}
+							style={{ color: "#fff", marginRight: "1rem" }}
+						/>
+						<FaLinkedin
+							size={30}
+							style={{ color: "#fff", marginRight: "1rem" }}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
---
+++ src/components/WorkCardData.jsx
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+
+const ProjectCardData = [
+	{
+		imgsrc: pro1,
+		title: "Ecommerce Website Design",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		view: "https://www.yotube.com/watch?v=P8YuWEkTeuE",
+	},
+	{
+		imgsrc: pro2,
+		title: "LMS/Course Website Design",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		view: "https://www.yotube.com/watch?v=P8YuWEkTeuE",
+	},
+	{
+		imgsrc: pro3,
+		title: "Rymo Ecommerce Design",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		view: "https://www.yotube.com/watch?v=P8YuWEkTeuE",
+	},
+];
+
+export default ProjectCardData;
